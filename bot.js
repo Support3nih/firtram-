9be84d75a575902a17c9b62b24787ed428b2d4b0
Support3nih <44061131+Support3nih@users.noreply.github.com>
2018-11-14 21:59:10 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('firegamer');
+ 
+});
+ 
+
+client.on('message', message => {
+  var prefix = "-";
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "408374252291751976") return;
+
+  
+  if (message.content.startsWith(prefix + 'sw')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}**`)
+} 
+
+ 
+  if (message.content.startsWith(prefix + 'sl')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+} 
+
+if (message.content.startsWith(prefix + 'sa')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+
+if (message.content.startsWith(prefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}**`)
+} 
+if (message.content.startsWith(prefix + 'setplay')) {
+  client.user.setGame(argresult);
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Playing: **${argresult}`)
+} 
+
+
+
+});
+
+client.login(process.env.BOT_KEY);
